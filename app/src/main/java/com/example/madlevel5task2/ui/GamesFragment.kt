9608cfaab50e86349b4c5bc52de4a7a4b45e938f
Robin <dtr.robin09@gmail.com>
2020-12-17 23:48:11 +0100

    GameViewModel.kt and GameAdapter.kt made

--- app/src/main/java/com/example/madlevel5task2/ui/GamesFragment.kt
+++ app/src/main/java/com/example/madlevel5task2/ui/GamesFragment.kt
@@ -5,19 +5,29 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
 import com.example.madlevel5task2.R
+import com.example.madlevel5task2.model.Game
+import com.example.madlevel5task2.repository.GameRepository
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
  */
-class GameBacklogFragment : Fragment() {
+class GamesFragment : Fragment() {
+
+    private val viewModel: GameViewModel by viewModels()
+
+    private lateinit var gameRepository: GameRepository
+
+    private var gamesList = arrayListOf<Game>()
+    private var gamesAdapter = GameAdapter(gamesList)
 
     override fun onCreateView(
             inflater: LayoutInflater, container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_game_backlog, container, false)
+        return inflater.inflate(R.layout.fragment_games, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
